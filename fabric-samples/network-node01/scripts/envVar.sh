04a6794a9c042291ca5a1ddb0ff7de178e6faa11
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-13 20:53:54 +1100

    support 40 peer network

--- fabric-samples/network-node01/scripts/envVar.sh
+++ fabric-samples/network-node01/scripts/envVar.sh
@@ -54,16 +54,6 @@ export PEER0_ORG37_CA=${PWD}/organizations/peerOrganizations/org37.example.com/p
 export PEER0_ORG38_CA=${PWD}/organizations/peerOrganizations/org38.example.com/peers/peer0.org38.example.com/tls/ca.crt
 export PEER0_ORG39_CA=${PWD}/organizations/peerOrganizations/org39.example.com/peers/peer0.org39.example.com/tls/ca.crt
 export PEER0_ORG40_CA=${PWD}/organizations/peerOrganizations/org40.example.com/peers/peer0.org40.example.com/tls/ca.crt
-export PEER0_ORG41_CA=${PWD}/organizations/peerOrganizations/org41.example.com/peers/peer0.org41.example.com/tls/ca.crt
-export PEER0_ORG42_CA=${PWD}/organizations/peerOrganizations/org42.example.com/peers/peer0.org42.example.com/tls/ca.crt
-export PEER0_ORG43_CA=${PWD}/organizations/peerOrganizations/org43.example.com/peers/peer0.org43.example.com/tls/ca.crt
-export PEER0_ORG44_CA=${PWD}/organizations/peerOrganizations/org44.example.com/peers/peer0.org44.example.com/tls/ca.crt
-export PEER0_ORG45_CA=${PWD}/organizations/peerOrganizations/org45.example.com/peers/peer0.org45.example.com/tls/ca.crt
-export PEER0_ORG46_CA=${PWD}/organizations/peerOrganizations/org46.example.com/peers/peer0.org46.example.com/tls/ca.crt
-export PEER0_ORG47_CA=${PWD}/organizations/peerOrganizations/org47.example.com/peers/peer0.org47.example.com/tls/ca.crt
-export PEER0_ORG48_CA=${PWD}/organizations/peerOrganizations/org48.example.com/peers/peer0.org48.example.com/tls/ca.crt
-export PEER0_ORG49_CA=${PWD}/organizations/peerOrganizations/org49.example.com/peers/peer0.org49.example.com/tls/ca.crt
-export PEER0_ORG50_CA=${PWD}/organizations/peerOrganizations/org50.example.com/peers/peer0.org50.example.com/tls/ca.crt
 
 # Set OrdererOrg.Admin globals
 setOrdererGlobals() {
@@ -105,7 +95,7 @@ setGlobals() {
     export CORE_PEER_LOCALMSPID="Org5MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG5_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org5.example.com/users/Admin@org5.example.com/msp
-    export CORE_PEER_ADDRESS=${Node01Addr}:12051
+    export CORE_PEER_ADDRESS=${Node02Addr}:12051
   elif [ $USING_ORG -eq 6 ]; then
     export CORE_PEER_LOCALMSPID="Org6MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG6_CA
@@ -125,12 +115,12 @@ setGlobals() {
     export CORE_PEER_LOCALMSPID="Org9MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG9_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org9.example.com/users/Admin@org9.example.com/msp
-    export CORE_PEER_ADDRESS=${Node02Addr}:16051
+    export CORE_PEER_ADDRESS=${Node03Addr}:16051
   elif [ $USING_ORG -eq 10 ]; then
     export CORE_PEER_LOCALMSPID="Org10MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG10_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org10.example.com/users/Admin@org10.example.com/msp
-    export CORE_PEER_ADDRESS=${Node02Addr}:17051
+    export CORE_PEER_ADDRESS=${Node03Addr}:17051
   elif [ $USING_ORG -eq 11 ]; then
     export CORE_PEER_LOCALMSPID="Org11MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG11_CA
@@ -145,17 +135,17 @@ setGlobals() {
     export CORE_PEER_LOCALMSPID="Org13MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG13_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org13.example.com/users/Admin@org13.example.com/msp
-    export CORE_PEER_ADDRESS=${Node03Addr}:10051
+    export CORE_PEER_ADDRESS=${Node04Addr}:10051
   elif [ $USING_ORG -eq 14 ]; then
     export CORE_PEER_LOCALMSPID="Org14MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG14_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org14.example.com/users/Admin@org14.example.com/msp
-    export CORE_PEER_ADDRESS=${Node03Addr}:11051
+    export CORE_PEER_ADDRESS=${Node04Addr}:11051
   elif [ $USING_ORG -eq 15 ]; then
     export CORE_PEER_LOCALMSPID="Org15MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG15_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org15.example.com/users/Admin@org15.example.com/msp
-    export CORE_PEER_ADDRESS=${Node03Addr}:12051
+    export CORE_PEER_ADDRESS=${Node04Addr}:12051
   elif [ $USING_ORG -eq 16 ]; then
     export CORE_PEER_LOCALMSPID="Org16MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG16_CA
@@ -165,171 +155,121 @@ setGlobals() {
     export CORE_PEER_LOCALMSPID="Org17MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG17_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org17.example.com/users/Admin@org17.example.com/msp
-    export CORE_PEER_ADDRESS=${Node04Addr}:14051
+    export CORE_PEER_ADDRESS=${Node05Addr}:14051
   elif [ $USING_ORG -eq 18 ]; then
     export CORE_PEER_LOCALMSPID="Org18MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG18_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org18.example.com/users/Admin@org18.example.com/msp
-    export CORE_PEER_ADDRESS=${Node04Addr}:15051
+    export CORE_PEER_ADDRESS=${Node05Addr}:15051
   elif [ $USING_ORG -eq 19 ]; then
     export CORE_PEER_LOCALMSPID="Org19MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG19_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org19.example.com/users/Admin@org19.example.com/msp
-    export CORE_PEER_ADDRESS=${Node04Addr}:16051
+    export CORE_PEER_ADDRESS=${Node05Addr}:16051
   elif [ $USING_ORG -eq 20 ]; then
     export CORE_PEER_LOCALMSPID="Org20MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG20_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org20.example.com/users/Admin@org20.example.com/msp
-    export CORE_PEER_ADDRESS=${Node04Addr}:17051
+    export CORE_PEER_ADDRESS=${Node05Addr}:17051
   elif [ $USING_ORG -eq 21 ]; then
     export CORE_PEER_LOCALMSPID="Org21MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG21_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org21.example.com/users/Admin@org21.example.com/msp
-    export CORE_PEER_ADDRESS=${Node05Addr}:7051
+    export CORE_PEER_ADDRESS=${Node06Addr}:7051
   elif [ $USING_ORG -eq 22 ]; then
     export CORE_PEER_LOCALMSPID="Org22MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG22_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org22.example.com/users/Admin@org22.example.com/msp
-    export CORE_PEER_ADDRESS=${Node05Addr}:9051
+    export CORE_PEER_ADDRESS=${Node06Addr}:9051
   elif [ $USING_ORG -eq 23 ]; then
     export CORE_PEER_LOCALMSPID="Org23MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG23_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org23.example.com/users/Admin@org23.example.com/msp
-    export CORE_PEER_ADDRESS=${Node05Addr}:10051
+    export CORE_PEER_ADDRESS=${Node06Addr}:10051
   elif [ $USING_ORG -eq 24 ]; then
     export CORE_PEER_LOCALMSPID="Org24MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG24_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org24.example.com/users/Admin@org24.example.com/msp
-    export CORE_PEER_ADDRESS=${Node05Addr}:11051
+    export CORE_PEER_ADDRESS=${Node06Addr}:11051
   elif [ $USING_ORG -eq 25 ]; then
     export CORE_PEER_LOCALMSPID="Org25MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG25_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org25.example.com/users/Admin@org25.example.com/msp
-    export CORE_PEER_ADDRESS=${Node05Addr}:12051
+    export CORE_PEER_ADDRESS=${Node07Addr}:12051
   elif [ $USING_ORG -eq 26 ]; then
     export CORE_PEER_LOCALMSPID="Org26MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG26_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org26.example.com/users/Admin@org26.example.com/msp
-    export CORE_PEER_ADDRESS=${Node06Addr}:13051
+    export CORE_PEER_ADDRESS=${Node07Addr}:13051
   elif [ $USING_ORG -eq 27 ]; then
     export CORE_PEER_LOCALMSPID="Org27MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG27_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org27.example.com/users/Admin@org27.example.com/msp
-    export CORE_PEER_ADDRESS=${Node06Addr}:14051
+    export CORE_PEER_ADDRESS=${Node07Addr}:14051
   elif [ $USING_ORG -eq 28 ]; then
     export CORE_PEER_LOCALMSPID="Org28MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG28_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org28.example.com/users/Admin@org28.example.com/msp
-    export CORE_PEER_ADDRESS=${Node06Addr}:15051
+    export CORE_PEER_ADDRESS=${Node07Addr}:15051
   elif [ $USING_ORG -eq 29 ]; then
     export CORE_PEER_LOCALMSPID="Org29MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG29_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org29.example.com/users/Admin@org29.example.com/msp
-    export CORE_PEER_ADDRESS=${Node06Addr}:16051
+    export CORE_PEER_ADDRESS=${Node08Addr}:16051
   elif [ $USING_ORG -eq 30 ]; then
     export CORE_PEER_LOCALMSPID="Org30MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG30_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org30.example.com/users/Admin@org30.example.com/msp
-    export CORE_PEER_ADDRESS=${Node06Addr}:17051
+    export CORE_PEER_ADDRESS=${Node08Addr}:17051
   elif [ $USING_ORG -eq 31 ]; then
     export CORE_PEER_LOCALMSPID="Org31MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG31_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org31.example.com/users/Admin@org31.example.com/msp
-    export CORE_PEER_ADDRESS=${Node07Addr}:7051
+    export CORE_PEER_ADDRESS=${Node08Addr}:7051
   elif [ $USING_ORG -eq 32 ]; then
     export CORE_PEER_LOCALMSPID="Org32MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG32_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org32.example.com/users/Admin@org32.example.com/msp
-    export CORE_PEER_ADDRESS=${Node07Addr}:9051
+    export CORE_PEER_ADDRESS=${Node08Addr}:9051
   elif [ $USING_ORG -eq 33 ]; then
     export CORE_PEER_LOCALMSPID="Org33MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG33_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org33.example.com/users/Admin@org33.example.com/msp
-    export CORE_PEER_ADDRESS=${Node07Addr}:10051
+    export CORE_PEER_ADDRESS=${Node09Addr}:10051
   elif [ $USING_ORG -eq 34 ]; then
     export CORE_PEER_LOCALMSPID="Org34MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG34_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org34.example.com/users/Admin@org34.example.com/msp
-    export CORE_PEER_ADDRESS=${Node07Addr}:11051
+    export CORE_PEER_ADDRESS=${Node09Addr}:11051
   elif [ $USING_ORG -eq 35 ]; then
     export CORE_PEER_LOCALMSPID="Org35MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG35_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org35.example.com/users/Admin@org35.example.com/msp
-    export CORE_PEER_ADDRESS=${Node07Addr}:12051
+    export CORE_PEER_ADDRESS=${Node09Addr}:12051
   elif [ $USING_ORG -eq 36 ]; then
     export CORE_PEER_LOCALMSPID="Org36MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG36_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org36.example.com/users/Admin@org36.example.com/msp
-    export CORE_PEER_ADDRESS=${Node08Addr}:13051
+    export CORE_PEER_ADDRESS=${Node09Addr}:13051
   elif [ $USING_ORG -eq 37 ]; then
     export CORE_PEER_LOCALMSPID="Org37MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG37_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org37.example.com/users/Admin@org37.example.com/msp
-    export CORE_PEER_ADDRESS=${Node08Addr}:14051
+    export CORE_PEER_ADDRESS=${Node10Addr}:14051
   elif [ $USING_ORG -eq 38 ]; then
     export CORE_PEER_LOCALMSPID="Org38MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG38_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org38.example.com/users/Admin@org38.example.com/msp
-    export CORE_PEER_ADDRESS=${Node08Addr}:15051
+    export CORE_PEER_ADDRESS=${Node10Addr}:15051
   elif [ $USING_ORG -eq 39 ]; then
     export CORE_PEER_LOCALMSPID="Org39MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG39_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org39.example.com/users/Admin@org39.example.com/msp
-    export CORE_PEER_ADDRESS=${Node08Addr}:16051
+    export CORE_PEER_ADDRESS=${Node10Addr}:16051
   elif [ $USING_ORG -eq 40 ]; then
     export CORE_PEER_LOCALMSPID="Org40MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG40_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org40.example.com/users/Admin@org40.example.com/msp
-    export CORE_PEER_ADDRESS=${Node08Addr}:17051
-  elif [ $USING_ORG -eq 41 ]; then
-    export CORE_PEER_LOCALMSPID="Org41MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG41_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org41.example.com/users/Admin@org41.example.com/msp
-    export CORE_PEER_ADDRESS=${Node09Addr}:7051
-  elif [ $USING_ORG -eq 42 ]; then
-    export CORE_PEER_LOCALMSPID="Org42MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG42_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org42.example.com/users/Admin@org42.example.com/msp
-    export CORE_PEER_ADDRESS=${Node09Addr}:9051
-  elif [ $USING_ORG -eq 43 ]; then
-    export CORE_PEER_LOCALMSPID="Org43MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG43_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org43.example.com/users/Admin@org43.example.com/msp
-    export CORE_PEER_ADDRESS=${Node09Addr}:10051
-  elif [ $USING_ORG -eq 44 ]; then
-    export CORE_PEER_LOCALMSPID="Org44MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG44_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org44.example.com/users/Admin@org44.example.com/msp
-    export CORE_PEER_ADDRESS=${Node09Addr}:11051
-  elif [ $USING_ORG -eq 45 ]; then
-    export CORE_PEER_LOCALMSPID="Org45MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG45_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org45.example.com/users/Admin@org45.example.com/msp
-    export CORE_PEER_ADDRESS=${Node09Addr}:12051
-  elif [ $USING_ORG -eq 46 ]; then
-    export CORE_PEER_LOCALMSPID="Org46MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG46_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org46.example.com/users/Admin@org46.example.com/msp
-    export CORE_PEER_ADDRESS=${Node10Addr}:13051
-  elif [ $USING_ORG -eq 47 ]; then
-    export CORE_PEER_LOCALMSPID="Org47MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG47_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org47.example.com/users/Admin@org47.example.com/msp
-    export CORE_PEER_ADDRESS=${Node10Addr}:14051
-  elif [ $USING_ORG -eq 48 ]; then
-    export CORE_PEER_LOCALMSPID="Org48MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG48_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org48.example.com/users/Admin@org48.example.com/msp
-    export CORE_PEER_ADDRESS=${Node10Addr}:15051
-  elif [ $USING_ORG -eq 49 ]; then
-    export CORE_PEER_LOCALMSPID="Org49MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG49_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org49.example.com/users/Admin@org49.example.com/msp
-    export CORE_PEER_ADDRESS=${Node10Addr}:16051
-  elif [ $USING_ORG -eq 50 ]; then
-    export CORE_PEER_LOCALMSPID="Org50MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG50_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org50.example.com/users/Admin@org50.example.com/msp
     export CORE_PEER_ADDRESS=${Node10Addr}:17051
   else
     echo "================== ERROR !!! ORG Unknown =================="
