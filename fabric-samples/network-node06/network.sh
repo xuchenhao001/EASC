6165418bd36f8eb5015e1aac7b84f06a20079928
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-13 12:19:13 +1100

    finished 10 nodes

---
+++ fabric-samples/network-node06/network.sh
@@ -0,0 +1,352 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This script brings up a Hyperledger Fabric network for testing smart contracts
+# and applications. The test network consists of two organizations with one
+# peer each, and a single node Raft ordering service. Users can also use this
+# script to create a channel deploy a chaincode on the channel
+#
+# prepending $PWD/../bin to PATH to ensure we are picking up the correct binaries
+# this may be commented out to resolve installed version of tools if desired
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/../configtx
+export VERBOSE=false
+
+# Obtain CONTAINER_IDS and remove them
+# TODO Might want to make this optional - could clear other containers
+# This function is called when you bring a network down
+function clearContainers() {
+  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')
+  if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
+    echo "---- No containers available for deletion ----"
+  else
+    docker rm -f $CONTAINER_IDS
+  fi
+}
+
+# Delete any images that were generated as a part of this setup
+# specifically the following images are often left behind:
+# This function is called when you bring the network down
+function removeUnwantedImages() {
+  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*/) {print $3}')
+  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS
+  fi
+}
+
+# Versions of fabric known not to work with the test network
+BLACKLISTED_VERSIONS="^1\.0\. ^1\.1\. ^1\.2\. ^1\.3\. ^1\.4\."
+
+# Do some basic sanity checking to make sure that the appropriate versions of fabric
+# binaries/images are available. In the future, additional checking for the presence
+# of go or other items could be added.
+function checkPrereqs() {
+  ## Check if your have cloned the peer binaries and configuration files.
+  peer version > /dev/null 2>&1
+
+  if [[ $? -ne 0 ]]; then
+    echo "ERROR! Peer binary and configuration files not found.."
+    echo
+    echo "Follow the instructions in the Fabric docs to install the Fabric Binaries:"
+    echo "https://hyperledger-fabric.readthedocs.io/en/latest/install.html"
+    exit 1
+  fi
+  # use the fabric tools container to see if the samples and binaries match your
+  # docker images
+  LOCAL_VERSION=$(peer version | sed -ne 's/ Version: //p')
+  DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-tools:$IMAGETAG peer version | sed -ne 's/ Version: //p' | head -1)
+
+  echo "LOCAL_VERSION=$LOCAL_VERSION"
+  echo "DOCKER_IMAGE_VERSION=$DOCKER_IMAGE_VERSION"
+
+  if [ "$LOCAL_VERSION" != "$DOCKER_IMAGE_VERSION" ]; then
+    echo "=================== WARNING ==================="
+    echo "  Local fabric binaries and docker images are  "
+    echo "  out of  sync. This may cause problems.       "
+    echo "==============================================="
+  fi
+
+  for UNSUPPORTED_VERSION in $BLACKLISTED_VERSIONS; do
+    echo "$LOCAL_VERSION" | grep -q $UNSUPPORTED_VERSION
+    if [ $? -eq 0 ]; then
+      echo "ERROR! Local Fabric binary version of $LOCAL_VERSION does not match the versions supported by the test network."
+      exit 1
+    fi
+
+    echo "$DOCKER_IMAGE_VERSION" | grep -q $UNSUPPORTED_VERSION
+    if [ $? -eq 0 ]; then
+      echo "ERROR! Fabric Docker image version of $DOCKER_IMAGE_VERSION does not match the versions supported by the test network."
+      exit 1
+    fi
+  done
+
+  ## Check for fabric-ca
+  if [ "$CRYPTO" == "Certificate Authorities" ]; then
+
+    fabric-ca-client version > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+      echo "ERROR! fabric-ca-client binary not found.."
+      echo
+      echo "Follow the instructions in the Fabric docs to install the Fabric Binaries:"
+      echo "https://hyperledger-fabric.readthedocs.io/en/latest/install.html"
+      exit 1
+    fi
+    CA_LOCAL_VERSION=$(fabric-ca-client version | sed -ne 's/ Version: //p')
+    CA_DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-ca:$CA_IMAGETAG fabric-ca-client version | sed -ne 's/ Version: //p' | head -1)
+    echo "CA_LOCAL_VERSION=$CA_LOCAL_VERSION"
+    echo "CA_DOCKER_IMAGE_VERSION=$CA_DOCKER_IMAGE_VERSION"
+
+    if [ "$CA_LOCAL_VERSION" != "$CA_DOCKER_IMAGE_VERSION" ]; then
+      echo "=================== WARNING ======================"
+      echo "  Local fabric-ca binaries and docker images are  "
+      echo "  out of sync. This may cause problems.           "
+      echo "=================================================="
+    fi
+  fi
+}
+
+# Create Organziation crypto material using cryptogen or CAs
+function createOrgs() {
+
+  if [ -d "organizations/peerOrganizations" ]; then
+    rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+  fi
+
+  # Create crypto material using cryptogen
+  if [ "$CRYPTO" == "cryptogen" ]; then
+    which cryptogen
+    if [ "$?" -ne 0 ]; then
+      fatalln "cryptogen tool not found. exiting"
+    fi
+    infoln "Generate certificates using cryptogen tool"
+
+    infoln "Create Orgs Identities"
+
+    set -x
+    cryptogen generate --config=./organizations/crypto-config.yaml --output="organizations"
+    res=$?
+    { set +x; } 2>/dev/null
+    if [ $res -ne 0 ]; then
+      fatalln "Failed to generate certificates..."
+    fi
+
+  fi
+
+  echo
+  echo "Generate CCP files for Orgs"
+  ./organizations/ccp-generate.sh
+}
+
+# Generate orderer system channel genesis block.
+function createConsortium() {
+
+  which configtxgen
+  if [ "$?" -ne 0 ]; then
+    echo "configtxgen tool not found. exiting"
+    exit 1
+  fi
+
+  echo "#########  Generating Orderer Genesis block ##############"
+
+  # Note: For some unknown reason (at least for now) the block file can't be
+  # named orderer.genesis.block or the orderer will fail to launch!
+  set -x
+  configtxgen -profile TwoOrgsOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate orderer genesis block..."
+    exit 1
+  fi
+}
+
+# Bring up the peer and orderer nodes using docker compose.
+function networkUp() {
+
+  checkPrereqs
+  # generate artifacts if they don't exist
+  if [ ! -d "organizations/peerOrganizations" ]; then
+    createOrgs
+    createConsortium
+  fi
+
+  COMPOSE_FILES="-f ${COMPOSE_FILE_BASE}"
+
+  if [ "${DATABASE}" == "couchdb" ]; then
+    COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_COUCH}"
+  fi
+
+  IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILES} up -d 2>&1
+
+  docker ps -a
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to start network"
+    exit 1
+  fi
+}
+
+
+# Tear down running network
+function networkDown() {
+  docker-compose -f $COMPOSE_FILE_BASE -f $COMPOSE_FILE_COUCH -f $COMPOSE_FILE_CA down --volumes --remove-orphans
+  # Don't remove the generated artifacts -- note, the ledgers are always removed
+  if [ "$MODE" != "restart" ]; then
+    # Bring down the network, deleting the volumes
+    #Cleanup the chaincode containers
+    clearContainers
+    #Cleanup images
+    removeUnwantedImages
+    # remove orderer block and other channel configuration transactions and certs
+    rm -rf system-genesis-block/*.block organizations/peerOrganizations organizations/ordererOrganizations
+    ## remove fabric ca artifacts
+    rm -rf organizations/fabric-ca/org11/msp organizations/fabric-ca/org11/tls-cert.pem organizations/fabric-ca/org11/ca-cert.pem organizations/fabric-ca/org11/IssuerPublicKey organizations/fabric-ca/org11/IssuerRevocationPublicKey organizations/fabric-ca/org11/fabric-ca-server.db
+    rm -rf organizations/fabric-ca/org12/msp organizations/fabric-ca/org12/tls-cert.pem organizations/fabric-ca/org21/ca-cert.pem organizations/fabric-ca/org2/1IssuerPublicKey organizations/fabric-ca/org2/I1ssuerRevocationPublicKey organizations/fabric-ca/org2/fa1bric-ca-server.db
+    rm -rf organizations/fabric-ca/org13/msp organizations/fabric-ca/org13/tls-cert.pem organizations/fabric-ca/org3/1ca-cert.pem organizations/fabric-ca/org3/Is1suerPublicKey organizations/fabric-ca/org3/Issu1erRevocationPublicKey organizations/fabric-ca/org3/fabric1-ca-server.db
+    rm -rf organizations/fabric-ca/org14/msp organizations/fabric-ca/org14/tls-cert.pem organizations/fabric-ca/org4/c1a-cert.pem organizations/fabric-ca/org4/Issu1erPublicKey organizations/fabric-ca/org4/IssuerR1evocationPublicKey organizations/fabric-ca/org4/fabric-ca-1server.db
+    rm -rf organizations/fabric-ca/org15/msp organizations/fabric-ca/org15/tls-cert.pem organizations/fabric-ca/org5/ca1-cert.pem organizations/fabric-ca/org5/Issuer1PublicKey organizations/fabric-ca/org5/IssuerRevo1cationPublicKey organizations/fabric-ca/org5/fabric-ca-serv1er.db
+    rm -rf organizations/fabric-ca/org16/msp organizations/fabric-ca/org16/tls-cert.pem organizations/fabric-ca/org6/ca-1cert.pem organizations/fabric-ca/org6/IssuerPu1blicKey organizations/fabric-ca/org6/IssuerRevocat1ionPublicKey organizations/fabric-ca/org6/fabric-ca-server.d1b
+    rm -rf organizations/fabric-ca/org17/msp organizations/fabric-ca/org17/tls-cert.pem organizations/fabric-ca/org7/ca-c1ert.pem organizations/fabric-ca/org7/IssuerPubl1icKey organizations/fabric-ca/org7/IssuerRevocation1PublicKey organizations/fabric-ca/org7/fabric-ca-server.d1b
+    rm -rf organizations/fabric-ca/org18/msp organizations/fabric-ca/org18/tls-cert.pem organizations/fabric-ca/org8/ca-ce1rt.pem organizations/fabric-ca/org8/IssuerPublic1Key organizations/fabric-ca/org8/IssuerRevocationPub1licKey organizations/fabric-ca/org8/fabric-ca-server.d1b
+    rm -rf organizations/fabric-ca/org19/msp organizations/fabric-ca/org19/tls-cert.pem organizations/fabric-ca/org9/ca-cer1t.pem organizations/fabric-ca/org9/IssuerPublicKe1y organizations/fabric-ca/org9/IssuerRevocationPublic1Key organizations/fabric-ca/org9/fabric-ca-server.d1b
+    rm -rf organizations/fabric-ca/org20/msp organizations/fabric-ca/org20/tls-cert.pem organizations/fabric-ca/org10/ca-ce2t.pem organizations/fabric-ca/org10/IssuerPublicK2y organizations/fabric-ca/org10/IssuerRevocationPubli2Key organizations/fabric-ca/org10/fabric-ca-server.2b
+
+    # remove channel and script artifacts
+    rm -rf channel-artifacts log.txt fabcar.tar.gz fabcar
+
+  fi
+}
+
+# Obtain the OS and Architecture string that will be used to select the correct
+# native binaries for your platform, e.g., darwin-amd64 or linux-amd64
+OS_ARCH=$(echo "$(uname -s | tr '[:upper:]' '[:lower:]' | sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+# Using crpto vs CA. default is cryptogen
+CRYPTO="cryptogen"
+# timeout duration - the duration the CLI should wait for a response from
+# another container before giving up
+MAX_RETRY=5
+# default for delay between commands
+CLI_DELAY=3
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
+# use this as the default docker-compose yaml definition
+COMPOSE_FILE_BASE=docker/docker-compose-test-net.yaml
+# docker-compose.yaml file if you are using couchdb
+COMPOSE_FILE_COUCH=docker/docker-compose-couch.yaml
+# certificate authorities compose file
+COMPOSE_FILE_CA=docker/docker-compose-ca.yaml
+#
+# use golang as the default language for chaincode
+CC_SRC_LANGUAGE=golang
+# Chaincode version
+VERSION=1
+# default image tag
+IMAGETAG="latest"
+# default ca image tag
+CA_IMAGETAG="latest"
+# default database
+DATABASE="couchdb"
+
+# Parse commandline args
+
+## Parse mode
+if [[ $# -lt 1 ]] ; then
+  printHelp
+  exit 0
+else
+  MODE=$1
+  shift
+fi
+
+while [[ $# -ge 1 ]] ; do
+  key="$1"
+  case $key in
+  -c )
+    CHANNEL_NAME="$2"
+    shift
+    ;;
+  -r )
+    MAX_RETRY="$2"
+    shift
+    ;;
+  -d )
+    CLI_DELAY="$2"
+    shift
+    ;;
+  -l )
+    CC_SRC_LANGUAGE="$2"
+    shift
+    ;;
+  -v )
+    VERSION="$2"
+    shift
+    ;;
+  -i )
+    IMAGETAG="$2"
+    shift
+    ;;
+  -cai )
+    CA_IMAGETAG="$2"
+    shift
+    ;;
+  -verbose )
+    VERBOSE=true
+    shift
+    ;;
+  * )
+    echo
+    echo "Unknown flag: $key"
+    echo
+    printHelp
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+# Are we generating crypto material with this command?
+if [ ! -d "organizations/peerOrganizations" ]; then
+  CRYPTO_MODE="with crypto from '${CRYPTO}'"
+else
+  CRYPTO_MODE=""
+fi
+
+# Determine mode of operation and printing out what we asked for
+if [ "$MODE" == "up" ]; then
+  echo "Starting nodes with CLI timeout of '${MAX_RETRY}' tries and CLI delay of '${CLI_DELAY}' seconds and using database '${DATABASE}' ${CRYPTO_MODE}"
+  echo
+elif [ "$MODE" == "createChannel" ]; then
+  echo "Creating channel '${CHANNEL_NAME}'."
+  echo
+  echo "If network is not up, starting nodes with CLI timeout of '${MAX_RETRY}' tries and CLI delay of '${CLI_DELAY}' seconds and using database '${DATABASE} ${CRYPTO_MODE}"
+  echo
+elif [ "$MODE" == "down" ]; then
+  echo "Stopping network"
+  echo
+elif [ "$MODE" == "restart" ]; then
+  echo "Restarting network"
+  echo
+elif [ "$MODE" == "deployCC" ]; then
+  echo "deploying chaincode on channel '${CHANNEL_NAME}'"
+  echo
+else
+  printHelp
+  exit 1
+fi
+
+if [ "${MODE}" == "up" ]; then
+  networkUp
+elif [ "${MODE}" == "down" ]; then
+  networkDown
+elif [ "${MODE}" == "restart" ]; then
+  networkDown
+  networkUp
+else
+  exit 1
+fi
+
