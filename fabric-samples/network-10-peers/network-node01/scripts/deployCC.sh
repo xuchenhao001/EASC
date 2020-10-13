e9fa6ee8f82f77117af69bc4ea78d00220d45f0f
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-14 00:10:41 +1100

    fix path for chaincode

--- fabric-samples/network-10-peers/network-node01/scripts/deployCC.sh
+++ fabric-samples/network-10-peers/network-node01/scripts/deployCC.sh
@@ -17,10 +17,10 @@ FABRIC_CFG_PATH=$PWD/../configtx/
 
 if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang" ] ; then
   CC_RUNTIME_LANGUAGE=golang
-  CC_SRC_PATH="../chaincode/fabcar/go/"
+  CC_SRC_PATH="../../chaincode/fabcar/go/"
 
   echo Vendoring Go dependencies ...
-  pushd ../chaincode/fabcar/go
+  pushd ../../chaincode/fabcar/go
   GO111MODULE=on go mod vendor
   popd
   echo Finished vendoring Go dependencies
