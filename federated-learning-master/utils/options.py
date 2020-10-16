c2d9a0655adca269a41ef1fd7a52cc12f66fe4ab
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-17 10:25:18 +1100

    bug fix for hyper option

--- federated-learning-master/utils/options.py
+++ federated-learning-master/utils/options.py
@@ -26,6 +26,7 @@ def args_parser():
     parser.add_argument('--num_filters', type=int, default=32, help="number of filters for conv nets")
     parser.add_argument('--max_pool', type=str, default='True',
                         help="Whether use max pooling rather than strided convolutions")
+    parser.add_argument('--hyper', type=float, default=0.3, help='hypermeter alpha')
 
     # other arguments
     parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset")
