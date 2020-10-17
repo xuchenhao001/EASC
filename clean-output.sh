ffef965d2bafdfed2e95dfee3f9bb5f5da20119c
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-18 00:23:51 +1100

    stop bring up python process when network up

--- clean-output.sh
+++ clean-output.sh
@@ -3,14 +3,8 @@
 
 AllNodesAddrs=(10.137.3.70 10.137.3.71 10.137.3.68 10.137.3.20 10.137.3.69 10.137.3.6 10.137.3.23 10.137.3.90 10.137.3.91 10.137.3.88)
 
-rm -rf output/
-mkdir -p output/
-
-cp federated-learning-master/result-record_*.txt output/
-
 for i in ${!AllNodesAddrs[@]}; do
   index=$(printf "%02d" $((i+2)))
-  # scp ubuntu@${AllNodesAddrs[$i]}:~/EASC/federated-learning-master/result-record_*.txt output/
   ssh ubuntu@${AllNodesAddrs[$i]} "rm -f ~/EASC/federated-learning-master/result-record_*.txt"
 done
 
