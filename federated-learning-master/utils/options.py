2010bcc4f50205444ed3fe315e85d88746b4aaeb
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-23 15:36:13 +1100

    update to mnist mlp

--- federated-learning-master/utils/options.py
+++ federated-learning-master/utils/options.py
@@ -18,8 +18,8 @@ def args_parser():
     parser.add_argument('--split', type=str, default='user', help="train-test split type, user or sample")
 
     # model arguments
-    parser.add_argument('--model', type=str, default='cnn', help='model name')
-    # parser.add_argument('--model', type=str, default='mlp', help='model name')
+    # parser.add_argument('--model', type=str, default='cnn', help='model name')
+    parser.add_argument('--model', type=str, default='mlp', help='model name')
     parser.add_argument('--kernel_num', type=int, default=9, help='number of each kind of kernel')
     parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
                         help='comma-separated kernel size to use for convolution')
@@ -30,8 +30,8 @@ def args_parser():
     parser.add_argument('--hyper', type=float, default=0.3, help='hypermeter alpha')
 
     # other arguments
-    parser.add_argument('--dataset', type=str, default='cifar', help="name of dataset")
-    # parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset")
+    # parser.add_argument('--dataset', type=str, default='cifar', help="name of dataset")
+    parser.add_argument('--dataset', type=str, default='mnist', help="name of dataset")
     parser.add_argument('--iid', action='store_true', help='whether i.i.d or not')
     parser.add_argument('--num_classes', type=int, default=10, help="number of classes")
     parser.add_argument('--num_channels', type=int, default=1, help="number of channels of imges")
