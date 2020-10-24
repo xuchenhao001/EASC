39f385735d6580f3b1da03438839d1241e86d0d8
xuchenhao001 <xu.chenhao@hotmail.com>
2020-10-24 15:20:17 +1100

    update to 50 peers

--- federated-learning-master/utils/options.py
+++ federated-learning-master/utils/options.py
@@ -8,7 +8,7 @@ def args_parser():
     parser = argparse.ArgumentParser()
     # federated arguments
     parser.add_argument('--epochs', type=int, default=50, help="rounds of training")
-    parser.add_argument('--num_users', type=int, default=20, help="number of users: K")
+    parser.add_argument('--num_users', type=int, default=50, help="number of users: K")
     parser.add_argument('--frac', type=float, default=1.0, help="the fraction of clients: C")
     parser.add_argument('--local_ep', type=int, default=5, help="the number of local epochs: E")
     parser.add_argument('--local_bs', type=int, default=10, help="local batch size: B")
