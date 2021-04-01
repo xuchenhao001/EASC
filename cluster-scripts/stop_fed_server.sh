eb75d11587e48834c04c1ed292db553a5ea2acb0
xuchenhao001 <xu.chenhao@hotmail.com>
2021-04-01 21:14:56 +1100

    stop raftd when stop fed_server,py

--- cluster-scripts/stop_fed_server.sh
+++ cluster-scripts/stop_fed_server.sh
@@ -6,5 +6,6 @@ for i in "${!PeerAddress[@]}"; do
   addrIN=(${PeerAddress[i]//:/ })
   
   ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[f]ed_server.py'|awk '{print \$2}')"
+  ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[h]raftd'|awk '{print \$2}')"
 done
 
