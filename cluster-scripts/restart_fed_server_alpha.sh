7053e8f68edfddc5fdd8ebee6042ad2fb34c6d8c
xuchenhao001 <xu.chenhao@hotmail.com>
2021-04-24 14:13:47 +1000

    stop raft when stop fed_server_alpha

--- cluster-scripts/restart_fed_server_alpha.sh
+++ cluster-scripts/restart_fed_server_alpha.sh
@@ -6,6 +6,7 @@ for i in "${!PeerAddress[@]}"; do
   addrIN=(${PeerAddress[i]//:/ })
   
   ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[f]ed_server_alpha.py'|awk '{print \$2}')"
+  ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[h]raftd'|awk '{print \$2}')"
   ssh ${HostUser}@${addrIN[0]} "(cd ~/EASC/federated-learning/; python3 -u fed_server_alpha.py) > ~/EASC/server_${addrIN[0]}.log 2>&1 &"
 done
 
