34417957181ed35af1752fea36c965b288b035d3
xuchenhao001 <xu.chenhao@hotmail.com>
2021-04-27 21:12:27 +1000

    start cluster processes with parameters

--- cluster-scripts/restart_fed_server_alpha.sh
+++ cluster-scripts/restart_fed_server_alpha.sh
@@ -7,6 +7,6 @@ for i in "${!PeerAddress[@]}"; do
   
   ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[f]ed_server_alpha.py'|awk '{print \$2}')"
   ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[h]raftd'|awk '{print \$2}')"
-  ssh ${HostUser}@${addrIN[0]} "(cd ~/EASC/federated-learning/; python3 -u fed_server_alpha.py) > ~/EASC/server_${addrIN[0]}.log 2>&1 &"
+  ssh ${HostUser}@${addrIN[0]} "(cd ~/EASC/federated-learning/; python3 -u fed_server_alpha.py --model=cnn --dataset=realworld --hyperpara=0.75) > ~/EASC/server_${addrIN[0]}.log 2>&1 &"
 done
 
--- cluster-scripts/restart_main_fed_localA.sh
+++ cluster-scripts/restart_main_fed_localA.sh
@@ -6,6 +6,6 @@ for i in "${!PeerAddress[@]}"; do
   addrIN=(${PeerAddress[i]//:/ })
   
   ssh ${HostUser}@${addrIN[0]} "kill -9 \$(ps -ef|grep '[m]ain_fed_localA.py'|awk '{print \$2}')"
-  ssh ${HostUser}@${addrIN[0]} "(cd ~/EASC/federated-learning/; python3 -u main_fed_localA.py) > ~/EASC/server_${addrIN[0]}.log 2>&1 &"
+  ssh ${HostUser}@${addrIN[0]} "(cd ~/EASC/federated-learning/; python3 -u main_fed_localA.py --model=cnn --dataset=realworld) > ~/EASC/server_${addrIN[0]}.log 2>&1 &"
 done
 
