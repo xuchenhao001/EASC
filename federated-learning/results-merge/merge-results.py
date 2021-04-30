6e131327365791106bbce99a6c35e689b8c0a749
xuchenhao001 <xu.chenhao@hotmail.com>
2021-04-30 14:24:06 +1000

    automated merge results

--- federated-learning/results-merge/merge-results.py
+++ federated-learning/results-merge/merge-results.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Python version: 3.6
-import re
 import numpy as np
+import os
+import re
 
-user_number = 10
 
-merged_file_name = "output/result-record_merged.txt"
-# read all of the files into lines[]
-lines = []
-for user_id in range(user_number):
-    filename = "output/" + "result-record_" + str(user_id+1) + ".txt"
-    with open(filename, 'r') as file:
-        lines.append(file.readlines())
-
-# start to process lines[]
-round_num = len(lines[0])
-
-with open(merged_file_name, 'w') as f:
-    pass
-for r in range(round_num):
-    round_gather_list = []
+def merge_files(user_number, merge_files_path):
+    # read all of the files into lines[]
+    lines = []
     for user_id in range(user_number):
-        record = lines[user_id][r]
-        record_trim = record[13:]
-        numbers_str = re.findall(r"[-+]?\d*\.\d+|\d+ ", record_trim)
-        numbers = [float(s) for s in numbers_str]
-        round_gather_list.append(numbers)
-    data = np.array(round_gather_list)
-    avg = np.average(data, axis=0).tolist()
-    avg_pretty = ["{0:0.2f}".format(i) for i in avg]
-    avg_str = "\t".join(avg_pretty)
-    with open(merged_file_name, 'a') as file:
-        file.write(avg_str + "\n")
+        filename = os.path.join(merge_files_path, "result-record_" + str(user_id + 1) + ".txt")
+        with open(filename, 'r') as file:
+            lines.append(file.readlines())
+
+    # start to process lines[]
+    round_num = len(lines[0])
+
+    # first time clean the file
+    merged_file_name = os.path.join(merge_files_path, "merged.txt")
+    open(merged_file_name, 'w').close()
+    for r in range(round_num):
+        round_gather_list = []
+        for user_id in range(user_number):
+            record = lines[user_id][r]
+            record_trim = record[13:]
+            numbers_str = re.findall(r"[-+]?\d*\.\d+|\d+ ", record_trim)
+            numbers = [float(s) for s in numbers_str]
+            round_gather_list.append(numbers)
+        data = np.array(round_gather_list)
+        avg = np.average(data, axis=0).tolist()
+        avg_pretty = ["{0:0.2f}".format(i) for i in avg]
+        avg_str = "\t".join(avg_pretty)
+        with open(merged_file_name, 'a') as file:
+            file.write(avg_str + "\n")
+
+
+def main():
+    # traverse directory, and list directories as dirs and files as files
+    # https://stackoverflow.com/questions/16953842/using-os-walk-to-recursively-traverse-directories-in-python
+    for dirpath, dirs, files in os.walk("./output"):
+        # print(dirpath, os.path.basename(dirpath))
+        if len(files) > 0 and files[0].endswith('.txt'):
+            resut_files = [file for file in files if not file.endswith('merged.txt')]
+            user_number = len(resut_files)
+            merge_files(user_number, dirpath)
+
+
+if __name__ == "__main__":
+    main()
